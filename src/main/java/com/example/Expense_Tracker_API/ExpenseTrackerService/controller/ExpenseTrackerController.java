578fee43072323902c6c3c970f94b867e13e0d05
Rameel Haroon <RameelHaroonOff@gmail.com>
2026-05-02 11:51:27 +0500

    Added a basic endpoint for testing purposes

---
+++ src/main/java/com/example/Expense_Tracker_API/ExpenseTrackerService/controller/ExpenseTrackerController.java
@@ -0,0 +1,21 @@
+package com.example.Expense_Tracker_API.ExpenseTrackerService.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(ExpenseTrackerController.BASE_URL)
+public class ExpenseTrackerController {
+    public static final String BASE_URL = "api/expenseTrackerService";
+    public static final String V1_TASK = "/v1/expense";
+    public static final String V1_CUSTOMER_ID = V1_TASK + "/{expenseId}";
+
+    @GetMapping(V1_TASK)
+    public String getTasks(Authentication authentication) {
+        return "Hello! This is the expense tracker app";
+    }
+}
