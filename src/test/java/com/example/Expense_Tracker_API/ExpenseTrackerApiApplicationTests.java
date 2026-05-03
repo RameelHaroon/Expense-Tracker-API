241c69bc4be255cada6c8e82141a91dd9f1877db
Rameel Haroon <RameelHaroonOff@gmail.com>
2026-05-03 20:38:52 +0500

    Fixed failing test with test containers

--- src/test/java/com/example/Expense_Tracker_API/ExpenseTrackerApiApplicationTests.java
+++ src/test/java/com/example/Expense_Tracker_API/ExpenseTrackerApiApplicationTests.java
@@ -2,9 +2,19 @@ package com.example.Expense_Tracker_API;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ExpenseTrackerApiApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
 
 	@Test
 	void contextLoads() {
